b2ea24e1ae592231f643886d935cb5de973275d8
dam <dam@172.26.81.176>
2019-12-12 11:50:32 +0100

    version 2

---
+++ src/proyecto1github/Proyecto1github.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto1github;
+
+/**
+ *
+ * @author dam
+ */
+public class Proyecto1github {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //primer mensaje
+        //segundo mensaje
+    }
+    
+}
